5453430bd6ee26897e3cff27ba3f744a279bb405
j_hara <j_hara@nst-co.com>
2022-12-26 16:43:04 +0900

    HTML diagramをpngから埋込みsvgに変更 PDF ページ番号が閉じ側になるのを修正 色が見にくいので変更

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 SET FIRMVER=3.0.4
 call asciidoctor-pdf ion.adoc -D  '%FIRMVER%' --theme my-theme.yml -a scripts=cjk -r asciidoctor-diagram -r ./mylib.rb
+call asciidoctor ion.adoc -o index.html -D '%FIRMVER%' -a toc=left -r asciidoctor-diagram -r ./mylib.rb -a imagesdir=image -a imagesoutdir='%FIRMVER%/image' -a diagram-format=svg -a diagram-svg-type=inline
 rd /s /q "%FIRMVER%\image"
-call asciidoctor ion.adoc -o index.html -D '%FIRMVER%' -a toc=left -r asciidoctor-diagram -r ./mylib.rb -a imagesdir=image -a imagesoutdir='%FIRMVER%/image'
 xcopy /Y /S /E image\ion "%FIRMVER%/image\ion\"
 rd /s /q "%FIRMVER%\.asciidoctor"
